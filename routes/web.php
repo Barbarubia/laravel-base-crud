68fc4cdcafbfcffb9c4484e7951709ff59586d2e
Barbarubia <dnl.desimone@gmail.com>
2022-05-09 16:40:37 +0200

    definzione routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomepageController@index')->name('homepage');
+
+Route::resource('/comics', 'ComicController');
