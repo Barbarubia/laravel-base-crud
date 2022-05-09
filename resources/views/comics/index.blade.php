cfed9225f4ab634385ae2d8787315ca237bfe14b
Barbarubia <dnl.desimone@gmail.com>
2022-05-09 17:48:08 +0200

    stile cards nella pagina comics/index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,9 +3,19 @@
 @section('pageTitle', 'Comics')
 
 @section('mainContent')
-    <ul>
-        @foreach ($comics as $comic)
-            <li><a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a></li>
-        @endforeach
-    </ul>
+    <div class="container mt-5 mb-5">
+        <div class="row g-4">
+            @foreach ($comics as $comic)
+                <div class="col-2">
+                    <div class="card h-100">
+                        <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Read more...</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,5 +3,7 @@
 @section('pageTitle', 'Homepage')
 
 @section('mainContent')
-    Homepage Content
+    <div class="container mt-5 mb-5">
+        <a href="{{ route('comics.index') }}" class="btn btn-primary fw-bold">Go to Comics Page</a>
+    </div>
 @endsection
