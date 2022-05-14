12ae2de93fd98091d58fde17e2aa944ce7ab1172
Barbarubia <dnl.desimone@gmail.com>
2022-05-14 14:06:18 +0200

    bonus: popup di conferma eliminazione

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,11 +7,38 @@
         <div class="row mb-5">
             <div class="col d-flex justify-content-end">
                 <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-primary mx-3">Edit comic</a>
-                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                {{-- <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-danger">Delete Comic</button>
-                </form>
+                </form> --}}
+
+                {{-- BONUS: popup per confermare eliminazione (usato modal di bootstrap) --}}
+                <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Delete comic</button>
+
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="staticBackdropLabel">You're deleting this comic!</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Are you sure?<br>Press YES to confirm, NO to cancel.
+                                </p>
+                            </div>
+                            <div class="modal-footer">
+                                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">YES</button>
+                                </form>
+                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">NO</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
         <div class="row">
